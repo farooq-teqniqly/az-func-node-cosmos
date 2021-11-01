7851f37a5a77196121cba9db9ed97a231ddadec2
Farooq Mahmud <farooq.mahmud@marel.com>
2021-11-01 08:51:07 -0700

    Updated create service principal script.

--- deploy/scripts/createServicePrincipal.ps1
+++ deploy/scripts/createServicePrincipal.ps1
@@ -1,8 +1,31 @@
 param(
     [Parameter(Mandatory = $true)]
-    [string] $ServicePrincipalName = $(throw "Missing service principal name argument")
+    [string] $ServicePrincipalName = $(throw "Missing service principal name argument"),
+
+    [Parameter(Mandatory = $true)]
+    [string] $SubscriptionName = $(throw "Missing subscription name argument")
 )
 
-az ad sp create-for-rbac `
-    -n $ServicePrincipalName `
-    --role Contributor
+$subscriptionId = az account show `
+    --subscription $SubscriptionName `
+    --query '{subscriptionId:id}' `
+| ConvertFrom-Json -AsHashtable
+
+if (!$?) {
+    throw "Subscription '$SubscriptionName' not found."
+}
+
+$sp = az ad sp create-for-rbac `
+    --name $ServicePrincipalName `
+    --role Contributor `
+    --query '{clientId:appId, clientSecret:password, tenantId:tenant}' `
+| ConvertFrom-Json -AsHashtable
+
+if (!$?) {
+    throw "There was an error creating the service principal."
+}
+
+$sp.Add("subscriptionId", $subscriptionId["subscriptionId"])
+
+ConvertTo-Json $sp
+
